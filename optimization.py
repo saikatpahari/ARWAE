59649bb556eba380a5eefc97e70d920e065c8e1f
saikatpahari <66266478+saikatpahari@users.noreply.github.com>
2023-11-30 13:18:52 +0530

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -1,12 +1,10 @@
-def is_satisfied(self, c_obj, iter):
+def is_satisfied(self, count, iter):
         """
-        Compute the satisfiability of the stopping criteria based on stopping
-        parameters and objective function value.
+        Stopping criteria based on stopping parameters. 
         
         Return logical value denoting factorization continuation. 
-        
-        :param c_obj: Current objective function value. 
-        :type c_obj: `float`
+        :param count: Current objective function value. 
+        :type count: `float`
         :param iter: Current iteration number. 
         :type iter: `int`
         """
@@ -14,64 +12,66 @@ def is_satisfied(self, c_obj, iter):
             return False
         if self.test_conv and iter % self.test_conv != 0:
             return True
-        if iter > 0 and c_obj < self.min_residuals * self.init_grad:
+        if iter > 0 and count < self.min_residuals * self.init_grad:
             return False
         if self.iterW == 0 and self.iterH == 0 and self.epsW + self.epsH < self.min_residuals * self.init_grad:
             # There was no move in this iteration
             return False
         return True
 
-def factorize(self):
+def mat_factorz(self):
         """
         Here we compute matrix factorization.
         """
-        for run in range(self.n_run):
-            self.W, self.H = self.seed.initialize(
+        for itr in range(self.n_itr):
+            self.U, self.H = self.seed.initialize(
                 self.V, self.rank, self.options)
-            self.gW = dot(self.W, dot(self.H, self.H.T)) - dot(
-                self.V, self.H.T)
-            self.gH = dot(dot(self.W.T, self.W), self.H) - dot(
-                self.W.T, self.V)
+            self.gU = dot(dot(self.M, self.V)+lamda(self.Z, self.H)) / dot(self.U,
+                self.V.T, self.V)    
+            self.gV = dot(self.M.T,self.U) / dot(self.V,self.U.T)
+                , self.U)
+            self.gH = dot(self.Z.T, self.U) / dot(dot(
+                self.V, self.U.T) self.U)
             self.init_grad = norm(vstack(self.gW, self.gH.T), p='fro')
             self.epsW = max(1e-3, self.min_residuals) * self.init_grad
             self.epsH = self.epsW
             # iterW and iterH are not parameters, as these values are used only
             # in first objective computation
-            self.iterW = 10
+            self.iterU = 10
+            self.iterV = 10
             self.iterH = 10
-            c_obj = sys.float_info.max
-            best_obj = c_obj if run == 0 else best_obj
+            count = sys.float_info.max
+            best_obj = count if itr == 0 else best_obj
             iter = 0
             if self.callback_init:
-                self.final_obj = c_obj
+                self.final_obj = count
                 self.n_iter = iter
                 mffit = mf_fit.Mf_fit(self)
                 self.callback_init(mffit)
-            while self.is_satisfied(c_obj, iter):
+            while self.is_satisfied(count, iter):
                 self.update()
                 iter += 1
-                c_obj = self.objective(
-                ) if not self.test_conv or iter % self.test_conv == 0 else c_obj
+                count = self.objective(
+                ) if not self.test_conv or iter % self.test_conv == 0 else count
                 if self.track_error:
-                    self.tracker.track_error(run, c_obj)
+                    self.tracker.track_error(itr, count)
             if self.callback:
-                self.final_obj = c_obj
+                self.final_obj = count
                 self.n_iter = iter
                 mffit = mf_fit.Mf_fit(self)
                 self.callback(mffit)
             if self.track_factor:
                 self.tracker.track_factor(
-                    run, W=self.W, H=self.H, final_obj=c_obj, n_iter=iter)
-            # if multiple runs are performed, fitted factorization model with
+                    itr, W=self.W, H=self.H, final_obj=count, n_iter=iter)
+            # if multiple itrs are performed, fitted factorization model with
             # the lowest objective function value is retained
-            if c_obj <= best_obj or run == 0:
-                best_obj = c_obj
+            if count <= best_obj or itr == 0:
+                best_obj = count
                 self.n_iter = iter
-                self.final_obj = c_obj
-                mffit = mf_fit.Mf_fit(copy.deepcopy(self))
-
-        mffit.fit.tracker = self.tracker
-        return mffit
+                self.final_obj = count
+                U = self.U
+        
+        return U
 
 def update(self):
         """Update basis and mixture matrix."""
